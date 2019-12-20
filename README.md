362848e7c82cbe3a4f8e9b1b4eb66d55caa4be91
nmuise <nmuise@sefas.com>
2019-12-20 15:05:21 -0500

    Added dictionary parsing ability, improved documentation

--- README.md
+++ README.md
@@ -1,22 +1,31 @@
-######################################## HOW TO USE CONFIG_PARSER.PY #######################################################
+---
+## HOW TO USE CONFIG_PARSER.PY
 
-########################################### CREATE CONFIG FILE ############################################################
-Create a .cnf file in a text editor
-Each section and setting must be written on a new line.
-At least one section is required.
-Settings may be used more than once but not in the same section.
-Spaces, tabs, and comments denoted by # are ignored.
+### 0. Quick reference for experienced users
+`python config_parser.py <config_file> <default_config_file> <output_path/filename>`
 
-Each configuration section is named:     [name]
-Each setting is written as:              setting_name = value
+`python config_parser.py` *(With the following files in the same directory as the script)*:
+* `drivers.cnf`
+* `driver_default_settings.cnf`
 
-EXAMPLE:
+### 1. Create config file
+* Create a .cnf file called `drivers.cnf` in a text editor
+    * You can give it any name, but if you don't call it this you'll have to remember it later.
+* Each section and setting must be written on a new line.
+* At least one section is required.
+    * Each configuration section is named:     [name]
+    * Each setting is written as:              setting_name = value
+* Settings may be used more than once but not in the same section.
+* Spaces, tabs, and comments denoted by # are ignored.
+
+#### EXAMPLE .cnf FILE:
+```
 [hp8]
- printer						= lwfi/bser
- fd								= unicode.dgs
- phy_img_compression			= rlencode
+ printer = lwfi/bser
+ fd = unicode.dgs
+ phy_img_compression = rlencode
  #Dont display
- phy_tech_top_margin			= 0 mm
+ phy_tech_top_margin = 0 mm
  output_unicode_mode=8bits
  
  hib1	= "&11H"/A4,S/A3,S/LETTER,LEGAL,S/EXECUTIVE,S
@@ -24,33 +33,31 @@ EXAMPLE:
 ## Section 2 ##
 
 [ps]
-iso_grey_level					= 0
-hob1							= "&11G"
+iso_grey_level = 0
+hob1 = "&11G"
 
-printer							= 1j4p/ps
+printer = 1j4p/ps
 margin_mode=1
+```
+
+### 2. Create default config file
+This file is used to define default values for certain sections.
+
+* Create a .cnf file called `driver_default_settings.cnf` in a text editor.
+    * You can give it any name, but if you don't call it this you'll have to remember it later.
+* If a setting in any section of the default file is not present in a section of the config file, the setting from the defualt file will be added to the configuration's output.
+* Format/content requirements of the default file are the same as the config file.
+    * There must be at least one section. 
+    * Additional sections are valid but are ignored within the script.
+    * Settings may have an empty value.
+        * Default values that are empty won't be added into config files.
+    * Settings may be used more than once but not in the same section.
 
 
-########################################### CREATE DEFAULT FILE ###########################################################
-Create another .cnf file in a text editor
+### 3. Run the script
+Use either of the following two commands in a command prompt in the same directory as the script:
+* `python config_parser.py <config_file> <default_config_file> <output_path/filename>`
 
-If a setting in any section of the default file is not present in a section of the config file, the setting from the defualt file will be added to the configuration's output.
-
-Conventions of the default file are the same as the config file. There must be at least one section. Additional sections are valid but are ignored within the script.
-
-Settings may have an empty value.
-Settings may be used more than once but not in the same section.
-
-
-########################################### RUNNING THE SCRIPT ############################################################
-
-Open the config_parser.py file in an editor
-
-At the top of the code, edit the INPUT FILES and OUTPUT FILE fields.
-CONFIG_FILEPATH = filepath (and file) to your config file
-DEFAULT_FILEPATH = filepath (and file) to your default file
-OUTPUT_FILEPATH = filepath (and file name) you desire your new .cnf to be created
-
-
-
-
+* `python config_parser.py` *(With the following files in the same directory as the script)*:
+    * `drivers.cnf`
+    * `driver_default_settings.cnf`
--- driver_settings/config_parser.py
+++ driver_settings/config_parser.py
@@ -1,33 +1,43 @@
 import configparser
+import sys
+import os
 
 
-# These files should be changed to your desired filepaths
-# INPUT FILES
-# .cnf file with configuration settings
-CONFIG_FILEPATH = 'C:/Users/bleblanc/Documents/Scripts/driver_settings/drivers.cnf'
-# .cnf file with default configuration settings to be used when a section within a config file section does not exist
-DEFAULT_FILEPATH = 'C:/Users/bleblanc/Documents/Scripts/driver_settings/driver_default_settings.cnf'
-
-# OUTPUT FILE
-OUTPUT_FILEPATH = 'C:/Users/bleblanc/Documents/Scripts/driver_settings/driver_settings_complete.cnf'
-
-
-# Get all parameters present across all sections
-# Each section uses all parameters but default if not assigned in cnf sections
-# Sets standard order of parameters to appear in each section
-def get_parameter_list(sect_list, config):
+def get_parameter_list(config):
+    """Returns a list of all unique parameters present across all sections in 
+    the given config.
+    """
     par_list = []
 
-    for section in sect_list:
+    for section in config.sections():
         for setting in config[section]:
             if setting not in par_list:
                 par_list.append(setting)
     return par_list
 
+def get_parameter_dict(config, parameter_list=None):
+    """Returns a dictionary mapping each config section to a list of all of its
+    settings.
+    """
+    if parameter_list is None:
+        parameter_list = get_parameter_list(config)
 
-# Print cnf
-def print_parsed_cnf(par_list, sect_list, config):
-    for section in sect_list:
+    parameter_dict = {}
+
+    for section in config.sections():
+        settings = []
+        for setting in config[section]:
+            settings.append(setting)
+        parameter_dict.update({section:settings})
+    
+    return parameter_dict
+
+
+def print_parsed_cnf(par_list, config):
+    """Pretty-prints the given parsed config file to the console.
+    """
+
+    for section in config.sections():
         print(section)
         for par in par_list:
             if par in config[section]:
@@ -38,11 +48,14 @@ def print_parsed_cnf(par_list, sect_list, config):
     return
 
 
-# default_config holds every setting
-# if config doesnt have a setting existing in default_config, use default_config's setting default and add to new_config
-# if default_config does not have a default setting, do not add setting to the new_config
-# return the edited new_config object
 def add_default_settings(config, def_config):
+    """Adds default values into the config for any fields which have values
+    defined in the default config, but which are not defined in the config. If
+    a setting exists in the default config but has no value, no new setting is
+    added to the newly-created config. 
+
+    Returns the appended config.
+    """
     new_config = configparser.ConfigParser()
     sects = config.sections()
     def_sects = def_config.sections()
@@ -73,42 +86,66 @@ def add_default_settings(config, def_config):
 
 
 def write_new_config(new_config, out_file):
+    """Writes out a file at the given output path/filename.
+    """
     with open(out_file, 'w') as newfile:
         new_config.write(newfile)
-        print("File: " + out_file)
+        print("New config created: " + out_file)
     return
 
 
-def main(file, def_file, out_file):
-    # Set file input/output conventions
-
-
+def main(config_file, default_file, out_file):
+    """Given the supplied paths to config, default, and output files, assemble
+    an appended version of config which contains parameters from the default
+    as necessary, and write out that file.
+    """
     # Create ConfigParse object for cnf file
     config = configparser.ConfigParser()
-    config.read(file)
+    config.read(config_file)
+
     # Create ConfigParse object for default settings cnf file
     default_config = configparser.ConfigParser()
-    default_config.read(def_file)
-
-    # sect_list = default_config.sections()
-    # par_list = get_parameter_list(sect_list, default_config)
-    # print_parsed_cnf(par_list,sect_list, default_config)
+    default_config.read(default_file)
 
     new_config = add_default_settings(config, default_config)
 
     write_new_config(new_config, out_file)
 
-    # sect_list = config.sections()
-
-    # par_list = get_parameter_list(sect_list, config)
-
-    # print_parsed_cnf(par_list, sect_list, config)
     return
 
 
 if __name__ == "__main__":
-    file = CONFIG_FILEPATH
-    def_file = DEFAULT_FILEPATH
-    out_file = OUTPUT_FILEPATH
-    main(file, def_file, out_file)
+    if len(sys.argv) == 1:
+        config_file = 'drivers.cnf'
+        default_file = 'driver_default_settings.cnf'
+        out_file = 'driver_settings_complete.cnf'
 
+        #Check if needed files exist
+        if not os.path.exists(config_file):
+            print("ERROR: no parameters were provided, but {0} did not lead to a file.".format(config_file))
+            exit(3)
+        if not os.path.exists(default_file):
+            print("ERROR: no parameters were provided, but {0} did not lead to a file.".format(default_file))
+            exit(3)
+
+        main(config_file, default_file, out_file)
+    else:
+        if len(sys.argv) == 4:
+            print("LEN 4")
+            config_file = sys.argv[1]
+            default_file = sys.argv[2]
+            out_file = sys.argv[3]
+
+            #check if supplied paths lead to files
+            if not os.path.exists(config_file):
+                print("ERROR: {0} does not lead to a readable file.".format(config_file))
+                exit(2)
+            if not os.path.exists(default_file):
+                print("ERROR: {0} does not lead to a readable file.".format(default_file))
+                exit(2)
+            
+            main(config_file, default_file, out_file)
+
+        if len(sys.argv) != 4:
+            print("ERROR: The usage of this command is:\r\n    $ python config_parser.py <config_file> <default_config_file> <output_path/filename>\r\nExactly three parameters must be provided.")
+            exit(1)
